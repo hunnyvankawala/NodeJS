38cb4ce17678814f42899c406b74fa6e374faf31
hunnyvankawala <37874276+hunnyvankawala@users.noreply.github.com>
2020-07-05 19:29:01 -0700

    HTTP calls and error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,31 @@
 const request = require('request')
 
-const url = "http://api.weatherstack.com/current?access_key=a2a56b5d94fff1edf87286912b781613&query=37.123,-122.897"
+const url = "http://api.weatherstack.com/current?access_key=a2a56b5d94fff1edf87286912b781613&query=37.123,-122.897&units=f"
 
 request({ url: url, json: true }, (error, response ) => {
-	console.log(response.body.current)
+	if(error){
+		console.log("Unable to reach weatherstack")
+	}
+	else if (response.body.error) {
+		console.log("Unable to find location")
+	}
+	else {
+		console.log(response.body.current.weather_descriptions[0])
+		console.log("It is currently " + response.body.current.temperature + " degrees out there")
+		console.log("It feels like " + response.body.current.feelslike + " degrees")
+	}
+})
+
+const url_geodata = "https://api.mapbox.com/geocoding/v5/mapbox.places/12what.json?access_token=pk.eyJ1IjoiaHVubnl2YW5rYXdhbGEiLCJhIjoiY2tjOXVpcmFiMXA2MDJxbW81dHFla3Y4ZCJ9.UbJNChCiXsdc9ZLvLE9PNQ&limit=1"
+
+request({ url: url_geodata, json: true}, (error, response) => {
+	if(error){
+		console.log("Unable to reach mapbox")
+	}
+	else if (response.body.features.length===0) {
+		console.log("Unable to find coordinates")
+	}
+	else{
+		console.log(response.body.features[0].center)
+	}
 })
